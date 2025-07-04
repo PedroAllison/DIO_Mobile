552da3d85d13f55b2109e1e76f7c02d07f50051b
Pedro Allison <pedro.fesilva45@gmail.com>
2025-07-04 19:24:59 -0300

    feat: Update Function | NEW DEPENDECES logSymbols and Chalck

--- 07-npm-packages-basic/src/index.js
+++ 07-npm-packages-basic/src/index.js
@@ -1,1 +1,13 @@
-console.log("hello pack")
+import chalk from "chalk";
+import logSymbols from "log-symbols"
+//-----------------------------------------------------
+console.log(chalk.blue.bgGray.bold("Hello World"))
+console.log(chalk.blue.bgGray.italic("Hello World"))
+console.log(chalk.red("Hello World"))
+console.log(chalk.green("Hello World"))
+console.log(chalk.blue.bgBlack("Hello"))
+//------------------------------------------------------
+console.log(logSymbols.success, chalk.green.bgBlackBright("servidor iniciado com sucesso"))
+console.log(logSymbols.error, chalk.red.bgWhite("servidor iniciado com problemas"))
+console.log(logSymbols.info, chalk.white.bgGray("servidor iniciado?"))
+console.log(logSymbols.warning, chalk.yellow.bgYellowBright("servidor necessita de atenção")) 
